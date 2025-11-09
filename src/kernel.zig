af16fe6617f75d23c8c70c91eca909e458ae71e1
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2025-11-09 17:37:53 +0100

    feature: Add simple putchar to output via SBI

--- src/kernel.zig
+++ src/kernel.zig
@@ -17,13 +17,10 @@ pub export fn _start() callconv(.naked) noreturn {
 }
 
 pub export fn kernel_main() callconv(.c) noreturn {
-    const start: *u8 = &__bss;
-    const end: *u8 = &__bss_end;
-    const len: usize = @intFromPtr(end) - @intFromPtr(start);
+    const msg = "\n\nHello World!\n";
+    for (msg) |c| putchar(c);
 
-    _ = memset(@ptrCast(start), 0, len);
-
-    while (true) {}
+    while (true) asm volatile ("wfi");
 }
 
 pub export fn memset(buf: *anyopaque, c: u8, n: usize) *anyopaque {
@@ -33,4 +30,45 @@ pub export fn memset(buf: *anyopaque, c: u8, n: usize) *anyopaque {
         p[i] = c;
     }
     return buf;
+}
+
+const SBIRet = struct {
+    err: isize,
+    value: isize,
+};
+
+pub fn sbi_call(
+    arg0: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+    arg5: usize,
+    fid: usize,
+    eid: usize,
+) SBIRet {
+    var out0: usize = 0;
+    var out1: usize = 0;
+
+    asm volatile ("ecall"
+        : [o0] "={a0}" (out0),
+          [o1] "={a1}" (out1),
+        : [i0] "{a0}" (arg0),
+          [i1] "{a1}" (arg1),
+          [i2] "{a2}" (arg2),
+          [i3] "{a3}" (arg3),
+          [i4] "{a4}" (arg4),
+          [i5] "{a5}" (arg5),
+          [i6] "{a6}" (fid),
+          [i7] "{a7}" (eid),
+        : .{ .memory = true });
+
+    return .{
+        .err = @bitCast(out0),
+        .value = @bitCast(out1),
+    };
+}
+
+pub fn putchar(ch: u8) void {
+    _ = sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
